c8148979e8819f41c5c0ba34bd562454193fee7b
flaberpengu <jacob.swales@gmail.com>
2020-09-11 19:04:56 +0100

    First commit.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ListeningSocketTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server myServer = new Server();
+            myServer.createSocket();
+            myServer.bindListenerAndListen();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ListeningSocketTest
+{
+    public class Server
+    {
+        private string data;
+        private byte[] bytes;
+        private IPHostEntry ipHostInfo;
+        private IPAddress ipAddress;
+        private IPEndPoint localEndPoint;
+        private Socket listener;
+
+        public Server()
+        {
+            ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            ipAddress = ipHostInfo.AddressList[0];
+            localEndPoint = new IPEndPoint(ipAddress, 11000);
+            data = null;
+            bytes = new byte[1024];
+        }
+
+        public void createSocket()
+        {
+            listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void bindListenerAndListen()
+        {
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.  
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.  
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.  
+                    while (true)
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Show the data on the console.  
+                    Console.WriteLine("Text received : {0}", data);
+
+                    // Echo the data back to the client.  
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
